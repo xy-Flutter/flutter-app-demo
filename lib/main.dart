b48cd09a243f1f4e467b9529311c32a9c0010fac
winfuture <insoul_net@qq.com>
2020-03-14 23:35:10 +0800

    弹性布局demo，使用flex&expanded

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,74 @@
-///
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-//  runApp(MyRowApp());
-  runApp(MyColumnApp());
-}
+void main() => runApp(MyFlexApp());
 
-///row(
-///children[Widget]: 布局内包含的组件
-///textDirection: children内组件摆放的顺序
-///mainAxisSize: 此row占水平方向大小空间,max:表示占满水平方向，min:表示水平宽度等于子组件总宽度
-///mainAxisAlignment: 如果mainAxisSize=min，没有意义，=max才有意义，此时，要参考textDirection初始方向，以textDirection为参考系
-///verticalDirection: 表示总抽的对齐方式，不是子组件在纵轴方向
-///crossAxisAlignment: 表示子组件在纵轴对齐方式，相对行底部
-///)
-class MyRowApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "MaterialApp标题",
-      theme: ThemeData.light(),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("脚手架标题"),
-            centerTitle: false, //标题是否居中，默认false
-          ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[Text(" hello world"), Text(" I am ok")],
-              ),
-              Row(
-                children: <Widget>[Text(" hello world "), Text(" I am ok")],
-              ),
-              Row(
-                textDirection: TextDirection.rtl,
-                mainAxisAlignment: MainAxisAlignment.start,
-                //TODO 此时应该是自下而上,为何不是在底部，配合crossAxisAlignment还不行？---想错了，这行无法跳到底部，而是指组件相对于行底部对齐
-                verticalDirection: VerticalDirection.up,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  Text(
-                    " hello world ",
-                    style: TextStyle(fontSize: 10),
-                  ),
-                  Text(" I am ok")
-                ],
-              ),
-            ],
-          )),
-    );
-  }
-}
-
-class MyColumnApp extends StatelessWidget {
+///弹性布局，很简单flex结合expanded
+class MyFlexApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("列布局标题"),
+          title: Text("Flex布局"),
         ),
         body: Container(
-          color: Colors.blue,
+          color: Colors.indigo,
           child: Column(
-//          mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[Text("hi"), Text("hello"), Text(" world")],
+            children: <Widget>[
+              Flex(
+                //水平方向的弹性布局
+                direction: Axis.horizontal,
+                children: <Widget>[
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: 30,
+                        color: Colors.yellow,
+                        child: Text("水平左边"),
+                      )),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      height: 30,
+                      color: Colors.cyanAccent,
+                      child: Text("水平右边"),
+                    ),
+                  )
+                ],
+              )
+              //分隔符，试试垂直方向的弹性布局
+              ,
+              Container(
+                //分隔符
+                padding: EdgeInsets.only(top: 10.0),
+                color: Colors.red,
+                height: 100, //3个分
+                //垂直三个方向
+                child: Flex(
+                  direction: Axis.vertical,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.cyanAccent,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        color: Colors.cyan,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
