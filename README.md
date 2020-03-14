5d46a2d2c3596c573432e024a3fa79cac9eedfa9
winfuture <insoul_net@qq.com>
2020-03-14 16:48:12 +0800

    线性布局row&column简单demo

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-# demo
+# 说明
+记录平时练习的一些代码&疑问
+# 疑问
+1. material的title是不是没有用？
+2. 
 
-A new Flutter application for practice.
+# 布局
+## 线性布局
+1. 水平布局row
+2. 垂直布局column
+# 容器
 
-## Getting Started
+# 组件
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,75 @@
-
-
+///
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
-//  runApp(MyApp2());
+//  runApp(MyRowApp());
+  runApp(MyColumnApp());
 }
 
-class MyApp extends StatelessWidget {
+///row(
+///children[Widget]: 布局内包含的组件
+///textDirection: children内组件摆放的顺序
+///mainAxisSize: 此row占水平方向大小空间,max:表示占满水平方向，min:表示水平宽度等于子组件总宽度
+///mainAxisAlignment: 如果mainAxisSize=min，没有意义，=max才有意义，此时，要参考textDirection初始方向，以textDirection为参考系
+///verticalDirection: 表示总抽的对齐方式，不是子组件在纵轴方向
+///crossAxisAlignment: 表示子组件在纵轴对齐方式，相对行底部
+///)
+class MyRowApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "MaterialApp标题",
-      theme: ThemeData.dark(),
+      theme: ThemeData.light(),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("脚手架标题"),
-        ),
-        body: Center(
-          child: Text("hell wolrd"),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("脚手架标题"),
+            centerTitle: false, //标题是否居中，默认false
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[Text(" hello world"), Text(" I am ok")],
+              ),
+              Row(
+                children: <Widget>[Text(" hello world "), Text(" I am ok")],
+              ),
+              Row(
+                textDirection: TextDirection.rtl,
+                mainAxisAlignment: MainAxisAlignment.start,
+                //TODO 此时应该是自下而上,为何不是在底部，配合crossAxisAlignment还不行？---想错了，这行无法跳到底部，而是指组件相对于行底部对齐
+                verticalDirection: VerticalDirection.up,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    " hello world ",
+                    style: TextStyle(fontSize: 10),
+                  ),
+                  Text(" I am ok")
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
 
-class MyApp2 extends StatelessWidget {
+class MyColumnApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "material app titile",
-      theme: ThemeData.dark(),
-      home: Center(
-        child: Text("hellworld"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("列布局标题"),
+        ),
+        body: Container(
+          color: Colors.blue,
+          child: Column(
+//          mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[Text("hi"), Text("hello"), Text(" world")],
+          ),
+        ),
       ),
     );
   }
